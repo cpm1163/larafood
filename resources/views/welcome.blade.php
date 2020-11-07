e3b9b80aa79eb149f1f30b65ee4b66c3b76056a0
cpm1163 <carlos.mahet@gmail.com>
2020-11-07 16:01:49 -0300

    terceiro commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div class="container mx-auto" style="background: red;">
+        <h1>Este H1</h1>
+        <h2>Este H2</h2>
+    </div>
+
+</body>
+</html>
